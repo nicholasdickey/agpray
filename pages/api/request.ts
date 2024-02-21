41f8baabca60b10f6369bead52da86a3a2627286
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-02-21 13:21:45 -0500

    Fix request regression.

--- pages/api/request.ts
+++ pages/api/request.ts
@@ -4,7 +4,7 @@ const api_key = process.env.LAKE_API_KEY;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
-  let { request} = req.body;
+  let { request} = req.query;
   console.log("request api",{request});
   const fetchResponse = await fetch(`${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/prayer/request?request=${request}&api_key=${api_key}`)
   const data = await fetchResponse.json();
