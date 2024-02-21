f1bebc0fdc143979061e5fa4be4d7e58f97a038f
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-02-21 11:18:13 -0500

    Fix mobile layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,7 @@ const Subtitle2 = styled.div`
   font-size: 0.8rem;
   font-weight: 500;
   text-align: center;
-  min-height:50px;
+  min-height:100px;
   //padding-top:3px;
   //padding-bottom:20px;
   @media (max-width: 900px) {
