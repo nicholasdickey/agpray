41d5824c4309d03d13af17559db543b504494187
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-02-21 12:53:17 -0500

    Fix iron-session version

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ const Welcome = styled.div<IProps>`
 `;
 const Subtitle = styled.div`
   color: var(--text);
-  font-size: 1.2rem;
+  font-size: 1.1rem;
   font-weight: 500;
   text-align: center;
   min-height:20px;
